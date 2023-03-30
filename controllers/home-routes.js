18f9261f312ddeaf8c29c4c32584febd8cdf8406
zehra <zehradastan@hotmail.com>
2023-03-30 18:22:47 +0100

    view folder updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,21 +46,21 @@ router.get('/post/:id', async (req, res) => {
   });
 
   router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-  
-    res.render("login");
-  });
-  
-  router.get("/signup", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-  
-    res.render("signup");
-  });
-  
-  module.exports = router;
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require("express-handlebars");
+const path = require('path');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,12 +24,12 @@ const sess = {
 };
 
 app.use(session(sess));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
